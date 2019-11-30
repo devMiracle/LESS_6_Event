13b6873c32b2f0b937c3c99b8c8c2dad0dfa10db
student <student@PC_105_1->
2019-11-30 16:59:47 +0200

    done!

---
+++ EventDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher();
+            Subscriber subscriber = new Subscriber();
+            Consumer consumer = new Consumer();
+            Consumer consumer2 = new Consumer();
+
+            subscriber.MakeASubscription(publisher, consumer);
+            subscriber.MakeASubscription(publisher, consumer2);
+            
+            publisher.Publish(new MagazineEventArgs() {
+                Title = "T1",
+                Content = "Lorem ipsum dolor",
+                PubDate = new DateTime()
+            });
+            Thread.Sleep(3000);
+            // publisher.consumers = null;
+            subscriber.DeleteSub(publisher, consumer2);
+            publisher.Publish(new MagazineEventArgs()
+            {
+                Title = "T2",
+                Content = "Lorem ipsum dolor 2",
+                PubDate = new DateTime()
+            });
+        }
+    }
+}
---
+++ EventDemo/Subscriber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    class Subscriber
+    {
+        public void MakeASubscription(Publisher publisher, Consumer consumer) {
+            publisher.consumers += consumer.Fetch; // = new EventHandler<MagazineEventArgs>(consumer.Fetch);
+            // .Add(consumer.Fetch);
+        }
+
+        public void DeleteSub(Publisher publisher, Consumer consumer)
+        {
+            publisher.consumers -= consumer.Fetch;
+        }
+    }
+}
